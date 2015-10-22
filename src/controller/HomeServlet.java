5205d3456b946d9787d2912a7eb0ed4a84a2b04e
RodrigoKM <rodrigokoythi@hotmail.com>
2015-10-22 16:20:43 -0200

    Servlet Home

--- src/controller/HomeServlet.java
+++ src/controller/HomeServlet.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,16 +12,17 @@ import javax.servlet.http.HttpServletResponse;
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public HomeServlet() {
-
-    }
+    public HomeServlet() {    }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+		RequestDispatcher rd = null;
+
+		rd = request.getRequestDispatcher("/index.jsp");
+		rd.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-
 }
