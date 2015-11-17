af0eb7f12e37a18cf0a4f2bbd6d7c296f3be0f67
RodrigoKM <rodrigokoythi@hotmail.com>
2015-11-16 22:42:46 -0200

    ws funcionando

--- src/service/WebService.java
+++ src/service/WebService.java
@@ -6,20 +6,22 @@ import javax.ws.rs.*;
 import com.thoughtworks.xstream.XStream;
 
 import bean.Produto;
+import bean.Usuario;
 import dao.ProdutoDAO;
+import dao.UsuarioDAO;
 
-@Path("/api")
-public class ProdutoService {
+@Path("/")
+public class WebService {
 	
 	//Objeto que converte o objeto em XML e vice-versa
 	private XStream conversor = new XStream();
-	private ProdutoDAO dao;
+	private ProdutoDAO daoProduto;
+	private UsuarioDAO daoUsuario;
 	
-	public ProdutoService(){
+	public WebService(){
 		try {
-			dao = new ProdutoDAO();
+			daoProduto = new ProdutoDAO();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}		
 	}
@@ -34,42 +36,29 @@ public class ProdutoService {
 	@Produces("application/xml")
 	public String pesquisarAluno(@PathParam("id") int id) throws Exception {
 		
-		//Obtendo o Aluno do DB com base no rgm 
-		Produto produto = dao.procurar(id);
+		Produto produto = daoProduto.procurar(id);
+		conversor.alias("produto", Produto.class);
 		
-		//Apelidando o nome "br.app.bean.Aluno" para "aluno"
-		conversor.alias("aluno", Produto.class);
-		
-		//Verifica se o aluno buscado existe.
 		if (produto != null)
-			//Envia os dados do aluno em XML
 			return conversor.toXML(produto);
 		else
-			//Envia o valor nulo em XML
 			return conversor.toXML(null);
 	}
 	
 	@POST
 	@Path("produto")
 	@Produces("application/xml")
-	public String inserirAluno(String xml) {
+	public String inserir(String xml) {
 		
-		//Apelidando o nome "br.app.bean.Aluno" para "aluno"
 		conversor.alias("produto", Produto.class);
 		
-		//Obtém o objeto Aluno a partir do XML
 		Produto produto = (Produto) conversor.fromXML(xml);
 		
-		//Insere no Banco e retorna TRUE ou FALSE mediante a execução
 		try {
-			dao.salvar(produto);
-			//Envia a resposta para o cliente.
-			//Envia o valor TRUE em XML em caso de inserção com sucesso.
+			daoProduto.salvar(produto);
 			return conversor.toXML(true);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-			//Envia o valor FALSE em XML em caso de erro de inserção.
 			return conversor.toXML(false);			
 		}
 		
@@ -78,17 +67,13 @@ public class ProdutoService {
 	@PUT
 	@Path("produto")
 	@Produces("application/xml")
-	public String atualizarAluno(String xml) {
-		
-		//Apelidando o nome "br.app.bean.Aluno" para "aluno"
+	public String atualizar(String xml) {
 		conversor.alias("produto", Produto.class);
 	
-		//Obtém o objeto Aluno através do XML
-		Produto produto = (Produto) conversor.fromXML(xml);			
+		Produto produto = (Produto) conversor.fromXML(xml);	
 		
-		//Atualiza os dados do aluno no banco
 		try {
-			dao.atualizar(produto);
+			daoProduto.atualizar(produto);
 			//Envia o valor true em caso de sucesso
 			return conversor.toXML(true);
 		} catch (Exception e) {
@@ -103,7 +88,7 @@ public class ProdutoService {
 	@Produces("application/xml")
 	public String excluirAluno(@PathParam("id") int id) {
 		try {
-			dao.excluir(id);
+			daoProduto.excluir(id);
 			return conversor.toXML(true);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -115,21 +100,25 @@ public class ProdutoService {
 	@Path("produtos")
 	@Produces("application/xml")
 	public String listarTodosAlunos() throws Exception {
-		//Apelidando o nome "br.app.bean.Aluno" para "aluno"
 		conversor.alias("produto", Produto.class);
 		
-		//Apelidando o nome "List" para "alunos"
 		conversor.alias("produtos", List.class);
 
-		//Obtendo a lista de Alunos do Banco
-		List<Produto> produtos = dao.listarProdutos();
+		List<Produto> produtos = daoProduto.listarProdutos();
 		
-		//Verificando se a lista existe
 		if (produtos != null)
-			//Envia esta lista convertida em XML
 			return conversor.toXML(produtos);
 		else
-			//Envia o valor nulo convertido para XML
 			return conversor.toXML(null);
+	}	
+	
+	@POST
+	@Path("logar")
+	public boolean logar(String xml) throws Exception {
+		conversor.alias("adm", Usuario.class);
+		
+		Usuario adm = (Usuario) conversor.fromXML(xml);			
+		
+		return daoUsuario.Logar(adm.getNome(), adm.getSenha());
 	}	
 }
